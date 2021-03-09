5b6b861994a2c138335d5495b7cfe03d17493e6a
Rajat Kumar <rajat1366@gmail.com>
2021-03-09 22:40:05 +0530

    testing 1

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -2,6 +2,6 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing");
+        System.out.println("Pipeline created");
     }
 }
