8057faa1c2d3346b685eca36d44a1f7385926bbe
Rajat Kumar <rajat1366@gmail.com>
2021-03-09 23:09:49 +0530

    testing 101

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -2,6 +2,6 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Calculator program 1");
+        System.out.println("Calculator program 10111");
     }
 }
